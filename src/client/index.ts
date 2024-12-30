552fdde338f6964bb1902f8e122739cb5e30b887
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-12-30 15:06:37 -0330

    Added env variables

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,20 @@
 import { io } from 'socket.io-client';
 
+const port = process.env.PORT
+const host = process.env.HOST
+const https = process.env.HTTPS
+const http = process.env.HTTP
+if (!port || !host || !https || !http) {
+  console.error('Missing required environment variables');
+}
+let protocol;
+if (process.env.ENV === 'dev') {
+  protocol = https;
+} else if (process.env.ENV === 'prod') {
+  protocol = http;
+}
+const base_url = `${protocol}://${host}:${port}`;
+
 function generateRandomAlphaString(separator: string, ...groups: number[]): string {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   let ns: string[] = [];
@@ -29,23 +44,23 @@ function prepareNamespace( hash: string, set_location: boolean ): string {
 const namespace = prepareNamespace(window.location.hash, true);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const socket = io(`https://localhost:3000/${namespace}`, { autoConnect: true});
+  const socket = io(`${base_url}/${namespace}`, { autoConnect: true});
 
-    console.log('Socket.IO client initialized');
+  console.log('Socket.IO client initialized');
 
-    socket.on('connect', () => {
-      console.log('Connected to server');
-    });
-
-    socket.on('connected peers', (peers: string[]) => {
-      console.log(`Connected peers: ${peers.join(', ')}`);
-    });
-
-    socket.on('disconnect', (reason) => {
-      console.log('Disconnected:', reason);
-    });
-
-    socket.on('connect_error', (error) => {
-      console.error('Connection error:', error);
-    });
+  socket.on('connect', () => {
+    console.log('Connected to server');
   });
+
+  socket.on('connected peers', (peers: string[]) => {
+    console.log(`Connected peers: ${peers.join(', ')}`);
+  });
+
+  socket.on('disconnect', (reason) => {
+    console.log('Disconnected:', reason);
+  });
+
+  socket.on('connect_error', (error) => {
+    console.error('Connection error:', error);
+  });
+});
